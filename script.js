d1934c3e822d11c4c678f58a56a7ebc3fcdfb5d9
Vidhya Parmeshwar Kadam <131935689+vidhyakadam@users.noreply.github.com>
2023-09-17 21:29:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function getFormvalue() {
     //Write your code here
+ const form = document.getElementById("myForm");
 
+  // Get the values of the First and Last name fields by their IDs
+  const firstName = form.elements["firstName"].value;
+  const lastName = form.elements["lastName"].value;
+
+  // Check if either of the fields is empty
+  if (firstName === "" || lastName === "") {
+    alert("Both First Name and Last Name are required.");
+  } else {
+    // Display the First and Last name using alert
+    alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
+  }
 }
